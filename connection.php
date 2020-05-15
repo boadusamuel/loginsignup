0799467d191215cfe6a647aec0f5d74998eaddbc
sam <boadusamuel@gmail.com>
2020-05-15 23:46:20 +0000

    set connection to postgres database

--- connection.php
+++ connection.php
@@ -1,2 +1,14 @@
 <?php
 
+
+$db_connection = pg_connect("host=ec2-34-206-252-187.compute-1.amazonaws.com port=5432 dbname=d2vdk81jnuo24v user=zfntnyjoanzebb password=634fd3b442c1c6ac93812ef028a8f3fe10485ca84494f39bbf26285a88ea3fa0");
+
+
+if($db_connection){
+
+    echo "Connection Okay";
+    
+}else{
+
+    echo "whoops";
+}
