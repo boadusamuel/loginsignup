5a5288c79f53b88b04c484bd84b30a4942becc34
sam <boadusamuel@gmail.com>
2020-05-15 23:28:38 +0000

    trying to connect to postgres

--- connection.php
+++ connection.php
@@ -1,18 +1,2 @@
 <?php
 
-$host = "localhost";
-$user = "postgres";
-$password = "F@rmdog222";
-$db_name = "panther";
-$port = "";
-
-$db_connection = pg_connect("host=$host dbname=$db_name user=$user password=$password");
-
-
-if($db_connection){
-
-    echo "Connection Okay";
-}else{
-
-    echo "whoops";
-}
